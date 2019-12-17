e3b7b0625a06a5a711c36da1ba583b3c1b37141b
cuzzuky87 <kazu_l_k@yahoo.co.jp>
2019-12-17 23:43:48 +0900

    add nested serializer

--- apiv1/serializers.py
+++ apiv1/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from todo import models
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id","last_login",)
+        read_only_fields = ('last_login',)
+
 class TodoSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
     class Meta:
         fields = (
             'id',
